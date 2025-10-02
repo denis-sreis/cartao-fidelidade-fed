77553fb2887470ccf33eb0b1beacfab312d5d819
Gustavo Borges <gustavoborgesalves00@gmail.com>
2025-10-02 09:47:11 -0300

    Add react router

---
+++ cartao-fidelidade/src/pages/Cadastro/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Cadastro.css'; 
+
+const Cadastro: React.FC = () => {
+return (
+    <div>
+    <h1>Página de Cadastro</h1>
+    <p>Este é o lugar para criar uma nova conta.</p>
+    </div>
+);
+}
+
+export default Cadastro;
