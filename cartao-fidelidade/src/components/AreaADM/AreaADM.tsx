9061c479fe0d058b3ef10773460badd82aee4b23
GabrielCampos2006 <bielfernandoferreira@gmail.com>
2025-10-22 13:51:36 -0300

    adição tela ADM

--- cartao-fidelidade/src/components/AreaADM/AreaADM.tsx
+++ cartao-fidelidade/src/components/AreaADM/AreaADM.tsx
@@ -4,14 +4,8 @@ function AreaADM(){
     return(
         <>
         <div className={'secaoADM'}>
-            <div className={'bQRCode'}>
-            <h1>Gerar QR Code</h1>
-            </div>
             <div className={'bCadastrar'}>
             <h1>Cadastrar Prêmio</h1>
-            </div>
-            <div className={'bConsultaFichas'}>
-            <h1>Consultar Fichas</h1>
             </div>
             <div className={'bConsultaCliente'}>
             <h1>Consultar Clientes</h1>
--- cartao-fidelidade/src/pages/Tela-principal/Admin/Index.tsx
+++ cartao-fidelidade/src/pages/Tela-principal/Admin/Index.tsx
@@ -5,6 +5,8 @@ import CartaoADM from '../../../components/CartaoADM/CartaoADM'
 import Navegacao from '../../../components/Navegacao/Navegacao'
 import MenuLateral from '../../../components/MenuLateral/MenuLateral'
 import AreaADM from '../../../components/AreaADM/AreaADM'
+import ListaPremiosADM from '../../../components/ListaPremios/ListaPremios'
+import BotaoQR from '../../../components/BotaoQR/BotaoQR'
 
 function TelaPrincipalADM() {
   const [menuAberto, setMenuAberto]=useState(false);
@@ -25,6 +27,16 @@ function TelaPrincipalADM() {
         <h1 className='textoPontoADMTP'>GORDUCHINHOS II</h1>
         <div className='cartaoADMTP'>
             <CartaoADM/>
+        </div>
+        <div className='botaoQRADMTP'>
+          <BotaoQR/>
+        </div>
+        <div className='premiosADMTP'>
+          <h1 className='headerPremios'>Prêmios</h1>
+          <h2 className='headerPremios'>Todos os Prêmios</h2>
+        </div>
+        <div className='listaPremiosADM'>
+            <ListaPremiosADM/>
         </div>
         <div className='ADMTP'>
           <h1 className='headerADM'>Administração</h1>
