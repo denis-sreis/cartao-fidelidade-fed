3f125683b903dd3196f056aab1f1057b5616a42d
denis-sreis <denisbrunoreis025@gmail.com>
2025-10-24 17:07:52 -0300

    Feat: ajustes no main

--- cartao-fidelidade/src/main.tsx
+++ cartao-fidelidade/src/main.tsx
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Index';
-import Cadastro from './pages/Cadastro/Index';
 import TelaPrincipalCliente from './pages/Tela-principal/Cliente/Index'
 import TelaPrincipalADM from './pages/Tela-principal/Admin/Index'
-import EsqueciSenha from './pages/EsqueciSenha/Index';
-import PerfilCliente from './pages/PerfilCliente/Index';
-import EditarDados from './pages/EditarDados/Index';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -16,12 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/principalCliente" element={<TelaPrincipalCliente />} />
         <Route path="/principalADM" element={<TelaPrincipalADM />} />
-        <Route path="/esqueci-senha" element={<EsqueciSenha />} />
-        <Route path="/perfil" element={<PerfilCliente />} />
-        <Route path="/editar-dados" element={<EditarDados />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
